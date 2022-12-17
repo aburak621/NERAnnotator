7e19d111624191cfb34cdd0fd04e719be5a0f604
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2022-12-17 20:27:57 +0300

    Basic functionality done

--- script.js
+++ script.js
@@ -1,32 +1,140 @@
-let p = document.querySelector('#text')
-let spans = document.querySelectorAll('p#text>span');
+let p = document.querySelector("#text")
+let spans = document.querySelectorAll("p#text>span");
+let jsonInput = document.querySelector("#jsonFile");
+let tagButtons = document.querySelector("div.tags");
 let firstElement;
 let secondElement;
+let startIndex;
+let endIndex;
+let selection = [];
+let selectionText = "";
+let tags = ["PERSON", "ORG"];
+let currentTag = tags[0];
+let entities = [];
 
-spans.forEach((element) => {
-    element.addEventListener('mousedown', (event) => {
-        firstElement = event.target;
-        console.log(event.target);
-    })
 
-    element.addEventListener('mouseup', (event) => {
-        secondElement = event.target;
-        console.log(event.target);
-        if (event.target.previousElementSibling.previousElementSibling == firstElement) {
-            console.log(event.target.previousElementSibling.previousElementSibling);
-            console.log("BRUUUUUUUUUH");
-        }
-    })
+addEventsToSpans();
+
+
+// Handle JSON upload.
+jsonInput.addEventListener("change", function () {
+    let fr = new FileReader();
+    fr.onload = function () {
+        let json = JSON.parse(fr.result);
+        // Update content and variables according to the JSON.
+        createHTMLFromJSON(json);
+        entities = json["entities"];
+        // Add new tags to the tags list
+        entities.forEach((entity) => {
+            if (!tags.includes(entity["tag"])) {
+                tags.push(entity["tag"]);
+            };
+        })
+        updateTagButtons();
+    }
+    fr.readAsText(this.files[0]);
 })
 
-function getSelectionNodes() {
-    let selection;
-    let selectionText = "";
-    if (!(window.getSelection() && window.getSelection()['type'] == 'Range')) { return; }
-    selection = document.getSelection();
-    console.log(selection.getRangeAt(0))
-    selectionText += selection.getRangeAt(0).cloneContents()
 
-    console.log(selection)
-    console.log(selectionText.innerHTML)
+// Events for selecting and tagging elements with mouse.
+function addEventsToSpans() {
+    spans = document.querySelectorAll("p#text>span");
+    spans.forEach((element) => {
+        element.addEventListener("mousedown", (event) => {
+            firstElement = event.target;
+        })
+
+        element.addEventListener("mouseup", (event) => {
+            secondElement = event.target;
+            getSelectedElements();
+            applyTag();
+            exportAsJSON();
+        })
+    })
+}
+
+function createHTMLFromJSON(json) {
+    let elements = json["text"].split(" ");
+    elements.forEach((element) => {
+        let span = document.createElement("span");
+        span.appendChild(document.createTextNode(element));
+        p.appendChild(span);
+        p.innerHTML += " ";
+    })
+    addEventsToSpans();
+}
+
+function updateTagButtons() {
+    tagButtons.innerHTML = "";
+    let isFirstItem = true;
+    tags.forEach((tag) => {
+        let tagButton = document.createElement("div");
+        tagButton.appendChild(document.createTextNode(tag));
+        tagButton.id = tag.toLowerCase();
+        tagButton.classList.add("tag");
+        tagButton.addEventListener("mousedown", (event) => {
+            currentTag = tagButton.innerText;
+            tagButtons.childNodes.forEach((button) => {
+                button.classList.remove("selected");
+            })
+            tagButton.classList.add("selected");
+        })
+        if (isFirstItem) {
+            tagButton.classList.add("selected");
+            isFirstItem = false;
+        }
+        tagButtons.appendChild(tagButton);
+    })
+    currentTag = tags[0];
+}
+
+// Apply currently selected tag to the highlighted elements. If it overlaps with other tags deleted those other tags.
+function applyTag() {
+    let overlappedEntitiesToBeDeleted = [];
+    for (let i = 0; i < entities.length; i++) {
+        if (Math.max(startIndex, entities[i]["startIndex"]) <= Math.min(endIndex, entities[i]["endIndex"])) {
+            overlappedEntitiesToBeDeleted.push(i);
+        }
+    }
+    overlappedEntitiesToBeDeleted.reverse().forEach((index) => {
+        entities.splice(index, 1);
+    })
+
+    // Add the new tag to entities.
+    let entity = { "text": selectionText, "startIndex": startIndex, "endIndex": endIndex, "tag": currentTag };
+    entities.push(entity);
+}
+
+// Get the selected elements and update the variables for tagging.
+function getSelectedElements() {
+    selection = [];
+    selectionText = "";
+
+    for (let i = 0; i < spans.length; i++) {
+        if (spans[i] == firstElement) {
+            startIndex = i;
+        } if (spans[i] == secondElement) {
+            endIndex = i;
+        }
+    }
+
+    if (startIndex > endIndex) {
+        [startIndex, endIndex] = [endIndex, startIndex];
+    }
+
+    for (let i = startIndex; i <= endIndex; i++) {
+        selection.push(spans[i]);
+        selectionText += spans[i].textContent + " ";
+    }
+    // Strip the punctuations and trim the corner space. Stripping punctuations might change!.
+    var regex = /[!"#$%&"()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    selectionText = selectionText.replace(regex, "");
+    selectionText = selectionText.trim();
+}
+
+// Create the final JSON to be exported.
+function exportAsJSON() {
+    json = { "text": p.innerText, "entities": entities };
+    json = JSON.stringify(json, null, 2);
+    console.log(json);
 }
